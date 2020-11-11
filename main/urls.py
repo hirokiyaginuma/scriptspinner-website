0e52740c0fe552013c4736b79ffd59c7f575fc11
Hiroki Yaginuma <hirokiyaginuma21@gmail.com>
2020-11-11 10:43:31 -0600

    Minor fix on the pages

--- main/urls.py
+++ main/urls.py
@@ -4,6 +4,7 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('about/', views.about, name='about'),
+    path('product/', views.product, name='product'),
     path('testimonial/', views.testimonial, name='testimonial'),
     path('team/', views.team, name='team'),
     path('contact/', views.contact, name='contact'),
--- main/views.py
+++ main/views.py
@@ -7,6 +7,9 @@ def index(request):
 def about(request):
     return render(request, 'about.html', {})
 
+def product(request):
+    return render(request, 'product.html', {})
+
 def testimonial(request):
     return render(request, 'testimonial.html', {})
 
